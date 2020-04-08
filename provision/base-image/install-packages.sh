83b89cc7f309578d1672da47ef5e8bc0dfdee1f8
Paul Norton <panorton@ucsd.edu>
2020-04-08 09:58:11 -0700

    Remove vixie-cron, added net-tools inplace of netstat, updated for Python3

--- provision/base-image/install-packages.sh
+++ provision/base-image/install-packages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo yum install epel-release -y
-sudo yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm -y
+sudo yum install https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm -y
 sudo yum -y update
 sudo yum install yum-plugin-priorities -y
 
-sudo yum install unzip tcpdump netstat wget tmux -y
-sudo yum install postfix vixie-cron crontabs man mlocate logrotate logwatch wget which -y
+sudo yum install unzip tcpdump net-tools wget tmux -y
+sudo yum install postfix crontabs man mlocate logrotate logwatch wget which -y
 sudo yum install bind-utils -y
 sudo yum install zlib-devel -y
 
@@ -17,14 +17,14 @@ sudo yum install gcc gcc-c++ -y
 sudo yum install git -y
 
 sudo yum install python-devel -y
-sudo yum install python-pip -y
+sudo yum install python3-pip -y
 
 sudo yum install dnsmasq -y
 
 sudo yum install salt-minion -y
 
-sudo pip uninstall tornado
-sudo pip install tornado==4.2.1
+sudo pip3 uninstall tornado
+sudo pip3 install tornado==4.2.1
 sudo yum install java -y
 
 #sudo  yum install collectd -y
